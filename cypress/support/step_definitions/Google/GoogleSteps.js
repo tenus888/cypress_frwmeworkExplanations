329712a87bf2bc341c19454c0d5c5ebd493da39f
tenus88 <diachenkoandrei1@gmail.com>
2021-10-13 10:36:31 -0400

    second commit

--- cypress/support/step_definitions/Google/GoogleSteps.js
+++ cypress/support/step_definitions/Google/GoogleSteps.js
@@ -5,3 +5,5 @@ import GooglePage from "../../pageObjects/GooglePage.js";
 When ('user uses google URL, user should be able to place on the main google page', () =>{
     GooglePage.navigateToTheGooglePAge();
 })
+
+// 12345
